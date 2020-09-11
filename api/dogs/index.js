707edeaa8a54c71755aaa2c29d9d3a7cb4eee095
Shiny <zhushiny@gmail.com>
2020-09-11 09:35:22 +0000

    Finished the dogs api function.

--- api/dogs/index.js
+++ api/dogs/index.js
@@ -1,13 +1,23 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.log('Returning list of dogs.');
+    const dogs = [
+        {name: 'Azure'},
+        {name: 'Sammy'},
+        {name: 'Roscoe'},
+        {name: 'Bailey'},
+        {name: 'Butch'},
+        {name: 'Pookie'},
+        {name: 'Keke'}
+    ]
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    context.res = {
+        body: {
+            dogs: dogs
+        },
+        header: {
+            "Content-Type": "application/json"
+        }
+    }
 }
