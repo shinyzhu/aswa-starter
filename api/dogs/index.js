d12327f9b16bb6d68fc41e22d213928d102c944e
Shiny <zhushiny@gmail.com>
2020-09-14 05:00:13 +0000

    trying update

---
+++ api/dogs/index.js
@@ -0,0 +1,21 @@
+module.exports = async function (context, req) {
+    context.log('Returning list of dogs.');
+    const dogs = [
+        {name: 'Azure'},
+        {name: 'Sammy'},
+        {name: 'Roscoe'},
+        {name: 'Bailey'},
+        {name: 'Butch'},
+        {name: 'Pookie'},
+        {name: 'Keke'}
+    ]
+
+    context.res = {
+        body: {
+            dogs: dogs
+        },
+        header: {
+            "Content-Type": "application/json"
+        }
+    }
+}
